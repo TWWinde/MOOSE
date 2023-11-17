7e7a1e10b4c3fb8fd0aa2fa0e74914b6d8f0fdd6
TWWinde <twwinde@gmail.com>
2023-11-17 07:55:34 +0100

    update

---
+++ segment.py
@@ -0,0 +1,7 @@
+from moosez.moosez import moose
+
+model_name = 'clin_ct_body'
+input_dir = '/misc/no_backups/s1449/Medical-Images-Synthesis/results/medicals/MOOSEv2_data'
+output_dir = '/misc/no_backups/s1449/Medical-Images-Synthesis/results/medicals/moose_segment_results'
+accelerator = 'cuda'
+moose(model_name, input_dir, output_dir, accelerator)
